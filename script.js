f6530b27aeac1324db8f3b77a83825658f3059ba
luis Colina <79709581+Silkiercomet@users.noreply.github.com>
2021-10-27 11:01:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -46,3 +46,87 @@ function shuffle(array) {
     }
   }
   fillCards()
+
+  //----------- funcionality --------------->
+  let matchTask = [], player1 = 0, player2 = 0,currentPlay = [], turn = 1;
+
+
+  const itsAMatch = () => {
+      //we can make an array that takes the two recent nodes if its a match it empyties the array
+      //other wise it call mismatch turns then around and the empties
+    if(matchTask.length === 2){
+       // matchTask[0] == matchTask[1] ? alert("match"):alert("no match")
+        if(matchTask[0] == matchTask[1] && turn % 2 !== 1){
+            ++player1
+            scoreboard()
+            point()
+            matchTask = []
+            currentPlay = []
+           // alert("match")
+        }else if(matchTask[0] == matchTask[1] && turn % 2 == 1){
+            ++player2
+            scoreboard()
+            point()
+            matchTask = []
+            currentPlay = []
+           // alert("match")
+        }else if(matchTask[0] !== matchTask[1]){
+            turn++;
+            matchTask = []
+            currentPlay = []
+            missMatch()
+            //alert("no match")
+        }
+        
+        
+    }
+    //picks the result
+
+  }
+  //turn the cards up || down
+  document.querySelectorAll(".card").forEach(a => {
+      a.addEventListener("click", function(){
+            //enviamos el valor obtenido abajo y lo evaluamos junto con el siguiente valor clickeado en 
+            //un array fuera de esta funcion
+          matchTask.push(a.childNodes[3].childNodes[1].childNodes[1].textContent)
+           a.childNodes[3].classList.toggle("cardback")
+           a.childNodes[1].classList.toggle("cardfront")
+           currentPlay.push(a.childNodes[3].id)
+           itsAMatch()
+           if(document.querySelectorAll(".card").length === ((player1+player2)*2)){
+            if(player1 < player2){
+                alert("player 1, you're winner")
+            }else if(player1 == player2){
+                alert("ties")
+            }else if(player2 < player1){
+                alert("player 2, you're winner")
+            }
+        }
+      })
+  })
+
+  //turn cards down on mismatch
+  function missMatch(){
+      setTimeout(function(){
+        document.querySelectorAll(".cardfront").forEach(a => a.classList.toggle("cardfront"))
+        document.querySelectorAll(".cardback").forEach(a => a.classList.toggle("cardback"))
+    }, 1000)
+  } 
+
+  //if its a match changes the classes of the already selected cards
+  function point(){
+      for(let ids of currentPlay){
+          document.querySelector(`#${ids}`).classList.add('solidback')
+          document.querySelector(`#${ids}`).classList.remove('cardback')
+
+
+          document.querySelector(`#${ids}`).previousElementSibling.classList.add('solidfront')
+          document.querySelector(`#${ids}`).previousElementSibling.classList.remove('cardfront')
+      }
+  }
+
+  //updates the content of the scoreboard
+  function scoreboard(){
+  document.querySelector(".player1").textContent = player2
+  document.querySelector(".player2").textContent = player1
+}
